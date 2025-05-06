553768c3eeb35b0e187556e79b12a9ae9bbb92f5
Niloy4001 <huzaifasikderniloy58bd@gmail.com>
2025-05-06 23:59:43 +0600

    today's practice

---
+++ Introduction of programming/Mod 14.5/char_to_ascii.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int char_to_ascii(char letter){
+    
+    return letter ;
+
+}
+
+int main(){
+
+    char c;
+    scanf("%c", &c);
+   const lt= char_to_ascii(c);
+   printf("%d", lt);
+    return 0;
+}
